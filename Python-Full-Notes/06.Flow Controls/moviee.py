57e53aa6c94164ce69e7907671876f3c042dc36c
kishana0427 <95524882+kishana0427@users.noreply.github.com>
2023-02-10 14:30:44 +0530

    Add files via upload

---
+++ Python-Full-Notes/06.Flow Controls/moviee.py
@@ -0,0 +1,7 @@
+#moviee.py
+tkt=input("Do u have Ticket:(yes/no):")
+if(tkt=="yes"):
+	print("Enter into theater")
+	print("watch movie")
+	print("Eat snacks!")
+print("\nGoto Home")
---
+++ Python-Full-Notes/06.Flow Controls/posnegzero.py
@@ -0,0 +1,9 @@
+#posnegzero.py
+n=int(input("Enter any number:"))
+if(n==0):
+	print("{} is ZERO".format(n))
+if(n>0):
+	print("{} is +Ve".format(n))
+if(n<0):
+	print("{} is -Ve".format(n))
+print("\nProgram execution completed")
